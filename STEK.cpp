47815ba25f981511cd686407d0d0d724603b2675
AlinaPoss <89980591+AlinaPoss@users.noreply.github.com>
2023-04-27 11:42:22 +0300

    Учла поправки

--- STEK.cpp
+++ STEK.cpp
@@ -16,26 +16,21 @@ public:
 
 template <typename Type>
 class Stack {
-    int sexy = 0; // типо size :)
-    int min_elem;
+    int sexy = 0; //
     Node<Type> *top;
 public:
     Stack() { top = nullptr; }
-    bool push(Type data)
+    void push(Type data)
     {
         Node<Type> *remind = new Node<Type>(data);
         if (!remind) {
             cout <<  "\nStack Overflow. You are stupid :)";
-            return 0;
+            exit(1);
         }
-        if (top == nullptr)
-            min_elem = data;
         remind -> data = data;
         remind -> prev = top;
         top = remind;
         sexy ++;
-        min_elem = min(min_elem, data);
-        return 1;
     }
     bool empty_()
     {
@@ -67,8 +62,46 @@ public:
     {
         return sexy;
     }
+};
+
+
+template <typename Type>
+class Stack_with_min {
+    Stack<Type> baze, miniki;
+public:
+    void push(Type data)
+    {
+        baze.push(data); // поместить заданный элемент в основной stack
+        if (miniki.size_() == 0) {
+            miniki.push(data);
+        } else {
+            if (data <= miniki.back_()){
+                miniki.push(data);
+            }
+        }
+    }
+    Type pop()
+    {
+        Type top = baze.pop();
+        if (top == miniki.back_()){
+            miniki.pop();
+        }
+        return top;
+    }
+    Type back_()
+    {
+        return baze.back_();
+    }
+    int size_()
+    {
+        return baze.size_();
+    }
     Type mini(){
-        return min_elem;
+        return miniki.back_();
+    }
+    bool empty_()
+    {
+        return baze.size_() == 0;
     }
 };
 
@@ -76,77 +109,66 @@ template <typename Type>
 class Queue {
     Stack<Type> s1, s2;
 public:
-    bool push(Type data)
+    void push(Type data)
     {
-        if (s1.empty_() == 1)
-            s1.push(data);
-        else
-            s2.push(data);
-        return 1;
+        s1.push(data);
     }
     int pop()
     {
         Type popy;
-        if (s1.size_() != 0) {
-            while (s1.size_() != 0){
+        if (s2.size_() != 0)
+            return s2.pop();
+        else {
+            while (s1.size_() != 0) {
                 s2.push(s1.pop());
             }
-            popy = s2.pop();
-        } else
-        if (s2.size_() != 0) {
-            while (s2.size_() != 0){
-                s1.push(s2.pop());
-            }
-            popy = s1.pop();
+            return s2.pop();
         }
-        return popy;
     }
     int size_()
     {
         return s1.size_() + s2.size_();
     }
-
 };
 
 
 int main(){
     // Тесты стека
     /*
-    Stack<int> m;           //ostatok:
-    assert(m.push(5) == 1); // 5
-    assert(m.push(4) == 1); // 5 4
-    assert(m.push(3) == 1); // 5 4 3
+    Stack_with_min<int> m;           //ostatok:
+    m.push(5);        // 5
+    m.push(4); // 5 4
+    m.push(3); // 5 4 3
     assert(m.size_() == 3);
     assert(m.mini() == 3);
     assert(m.pop() == 3);   // 5 4
     assert(m.size_() == 2);
     assert(m.back_() == 4);
     assert(m.pop() == 4);   // 5
-    assert(m.empty_() == 0);
+    assert(m.empty_() == false);
     assert(m.pop() == 5);   // empty
-    assert(m.empty_() == 1);
+    assert(m.empty_() == true);
+    m.push(-2);
+    assert(m.mini() == -2);
     */
 
     //Тесты очереди
     /*
     Queue<int> q;
-    assert(q.push(5) == 1);
-    assert(q.push(6) == 1);
-    assert(q.push(7) == 1);
+    q.push(5);
+    q.push(6);
+    q.push(7);
     assert(q.size_() == 3);
     assert(q.pop() == 5);
     assert(q.pop() == 6);
     assert(q.size_() == 1);
-    assert(q.push(8) == 1);
+    q.push(8);
     assert(q.pop() == 7);
     assert(q.pop() == 8);
     assert(q.size_() == 0);
-    assert(q.push(5) == 1);
-    assert(q.push(6) == 1);
-    assert(q.push(7) == 1);
+    q.push(5);
+    q.push(6);
+    q.push(7);
     assert(q.size_() == 3);
-    /*
-
-
-
+    */
 }
