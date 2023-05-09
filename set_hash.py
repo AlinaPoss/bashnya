ae473fa0cca97210dee61b4b3930d17d10bf8047
AlinaPoss <89980591+AlinaPoss@users.noreply.github.com>
2023-05-09 11:23:34 +0300

    Стек с хэшами
    Как-то примитивно получилось, сомневаюсь в правильности.
    Решила вспомнить ооп на питоне.

---
+++ set_hash.py
@@ -0,0 +1,57 @@
+from random import*
+
+class Node:
+    def __init__(self, k, x): #key, x
+        self.k = k
+        self.x = x
+        self.next = None
+
+class Set(object):
+    def __init__(self):
+        self.M = 10000
+        self.size = 0
+        self.table = [None] * self.M
+        
+    def insert(self, x):
+        #если индекс свободен, кладем туда значение
+        #если нет, все равно кладем, просто новое
+        M = self.M
+        i = self._hash(x)
+        while not(self.table[i] is None):
+            i = (i + i) % M
+        self.table[i] = x
+        
+    def exists(self, x):
+        M = self.M
+        i = self._hash(x)
+        while not(self.table[i] is None):
+            if self.table[i] == x:
+                return True
+            i = (i + i) % M
+        return False
+    
+    def delete(self, x):
+        M = self.M
+        i = self._hash(x)
+        while not(self.table[i] is None):
+            if self.table[i] == x:
+                self.table[i] = None
+                break
+            i = (i + i) % M
+    def doRehashing(): #для сета вроде не понадобился 
+        pass
+    
+    def _hash(self, key):
+        a = 257
+        p = 600
+        M = self.M
+        return (a*key % p) % M
+
+s = Set()
+s.insert(3)
+s.insert(5)
+assert s.exists(5) == True
+s.delete(5)
+assert s.exists(5) == False
+assert s.exists(4) == False
+assert s.exists(3) == True
